c48d79e12002688a4711466a0fe9b2b909046d8a
Antnamve <antnamve@gmail.com>
2024-06-25 08:26:30 +0300

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -94,7 +94,7 @@ export class AuthService {
 
 		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
 			httpOnly: true,
-			domain: 'localhost',
+			domain: '87.228.8.47',
 			expires: expiresIn,
 			secure: true,
 			sameSite: 'lax'
@@ -104,7 +104,7 @@ export class AuthService {
 	removeRefreshTokenFromResponse(res: Response) {
 		res.cookie(this.REFRESH_TOKEN_NAME, '', {
 			httpOnly: true,
-			domain: 'localhost',
+			domain: '87.228.8.47',
 			expires: new Date(0),
 			secure: true,
 			sameSite: 'lax'
