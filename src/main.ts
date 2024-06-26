29d0ab50006bfb89a409c7bac1095c038428dfc1
Antnamve <antnamve@gmail.com>
2024-06-26 17:32:42 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000', 'http://87.228.8.47:3000'],
+		origin: [
+			'http://localhost:3000',
+			'http://87.228.8.47:3000',
+			'http://87.228.8.47'
+		],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
